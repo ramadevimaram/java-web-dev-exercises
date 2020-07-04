561e3f95ccffdd4a0cfd638a3d234732cd87226c
ramadevimaram <ramamaram.java@gmail.com>
2020-07-04 11:41:46 -0400

    added Restaurant class

--- src/org/launchcode/java/studios/studio3/restaurant/Restaurant.java
+++ src/org/launchcode/java/studios/studio3/restaurant/Restaurant.java
@@ -1,6 +1,6 @@
 package org.launchcode.java.studios.studio3.restaurant;
 
-public class TestMenu {
+public class Restaurant {
     public static void main(String[] args) {
         // write your code here
 
